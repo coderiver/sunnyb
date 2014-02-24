5ee58d7d302d9e93aa629fcb23270fc3620f44e7
Valera19900 <kaktuz34@gmail.com>
2014-02-24 11:11:52 +0200

    upd

--- js/common.js
+++ js/common.js
@@ -1,9 +1,26 @@
 $(document).ready(function() {
-	$(document).ready(function() {
-$('#slider').cycle({ 
-    fx:      'scrollLeft', 
-    speed:    1000, 
-    timeout:  2000
-		});
+	$('#slider').cycle({ 
+	    fx:      'scrollLeft', 
+	    speed:    1000, 
+	    timeout:  5000
+	});
+	var progress = $('.progress'),
+	    slideshow = $( '.cycle-slideshow' );
+
+	slideshow.on( 'cycle-initialized cycle-before', function( e, opts ) {
+	    progress.stop(true).css( 'width', 0 );
+	});
+
+	slideshow.on( 'cycle-initialized cycle-after', function( e, opts ) {
+	    if ( ! slideshow.is('.cycle-paused') )
+	        progress.animate({ width: '100%' }, opts.timeout, 'linear' );
+	});
+
+	slideshow.on( 'cycle-paused', function( e, opts ) {
+	   progress.stop(); 
+	});
+
+	slideshow.on( 'cycle-resumed', function( e, opts, timeoutRemaining ) {
+	    progress.animate({ width: '100%' }, timeoutRemaining, 'linear' );
 	});
 });
